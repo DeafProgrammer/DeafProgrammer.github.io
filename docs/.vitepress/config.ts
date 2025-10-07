6c9278cbec1c74dde692d77b7823ea2676b5cc99
DeafProgrammer <James.P.Anderson19@outlook.com>
2025-10-06 19:53:25 -0500

    nav2

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,34 +2,43 @@ import { defineConfig } from "vitepress";
 const urlName = "https://DeafProgrammer.github.io/";
 
 // https://vitepress.vuejs.org/config/app-configs
-export default {
+export default defineConfig({
   title: "James Anderson's Spiritual Personality & Makeup",
   description: "Spiritual Personality & Makeup",
   themeConfig: {
     siteTitle: "James Anderson",
     nav: [
-      { text: "About Me", link: "/about-me", activeMatch: "/about-me" },
+      { text: "About Me", link: "/about-me", activeMatch: "^/about-me" },
       {
         text: "Spiritual Personality",
         link: "/spiritual-personality",
-        activeMatch: "/about-me",
+        activeMatch: "^/spiritual-personality",
       },
-      { text: "Better Than Before", link: "/better-than-before" },
+      {
+        text: "Better Than Before",
+        link: "/better-than-before",
+        activeMatch: "^/better-than-before",
+      },
     ],
     sidebar: {
       "/about-me": [
         {
-          text: "About me",
+          text: "About Me",
+          items: [{ text: "About Me", link: "/about-me" }],
         },
       ],
       "/spiritual-personality": [
         {
           text: "Spiritual Personality",
+          items: [
+            { text: "Spiritual Personality", link: "/spiritual-personality" },
+          ],
         },
       ],
       "/better-than-before": [
         {
           text: "Better Than Before",
+          items: [{ text: "Better Than Before", link: "/better-than-before" }],
         },
       ],
     },
@@ -41,4 +50,4 @@ export default {
     ],
   },
   base: "/",
-};
+});

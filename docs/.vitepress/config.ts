2679e734fa8c333aaf00c2814ef8e2cf941cbd36
DeafProgrammer <James.P.Anderson19@outlook.com>
2025-10-06 19:46:56 -0500

    Navtest2

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,13 +1,43 @@
 import { defineConfig } from "vitepress";
+const urlName = "https://DeafProgrammer.github.io/";
 
 // https://vitepress.vuejs.org/config/app-configs
 export default {
-  title: "DeafProgrammer",
+  title: "James Anderson's Spiritual Personality & Makeup",
+  description: "Spiritual Personality & Makeup",
   themeConfig: {
+    siteTitle: "James Anderson",
     nav: [
-      { text: "About Me", link: "/about-me" },
-      { text: "Spiritual Personality", link: "/spiritual-personality" },
+      { text: "About Me", link: "/about-me", activeMatch: "/about-me" },
+      {
+        text: "Spiritual Personality",
+        link: "/spiritual-personality",
+        activeMatch: "/about-me",
+      },
       { text: "Better Than Before", link: "/better-than-before" },
+    ],
+    sidebar: {
+      "/about-me": [
+        {
+          text: "About me",
+        },
+      ],
+      "/spiritual-personality": [
+        {
+          text: "Spiritual Personality",
+        },
+      ],
+      "/better-than-before": [
+        {
+          text: "Better Than Before",
+        },
+      ],
+    },
+    socialLinks: [
+      {
+        icon: "github",
+        link: "https://github.com/DeafProgrammer/DeafProgrammer.github.io",
+      },
     ],
   },
   base: "/",
